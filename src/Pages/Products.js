f605823caad64c183e0570959a9befb601e04832
ny4899 <ny4899@gmail.com>
2022-07-27 13:11:35 +0530

    Added footer information section in product page

--- src/Pages/Products.js
+++ src/Pages/Products.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import ProductsWrapper from '../Layouts/Products/ProductsWrapper/ProductsWrapper'
 import CategoryFilterNav from '../Layouts/Products/CategoryFilterNav/CategoryFilterNav'
-
+import InformationSection from '../Layouts/Home/InformationSection'
 const Products = () => {
   return (
     <>
     <ProductsWrapper />
     <CategoryFilterNav />
+    <InformationSection />
     </>
   )
 }
